7da2169a2ae3345856748805e4021b08b06319dd
lauri000 <kajakko.lauri@gmail.com>
2026-04-10 14:01:26 +0300

    Fix chat composer keyboard insets on Pixels

--- app/src/main/java/social/innode/ndr/demo/ui/screens/ChatScreen.kt
+++ app/src/main/java/social/innode/ndr/demo/ui/screens/ChatScreen.kt
@@ -5,9 +5,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -81,6 +83,7 @@ fun ChatScreen(
 
     Scaffold(
         containerColor = MaterialTheme.colorScheme.background,
+        contentWindowInsets = WindowInsets(0, 0, 0, 0),
         topBar = {
             IrisTopBar(
                 title =
@@ -317,6 +320,7 @@ private fun ComposerBar(
         modifier =
             Modifier
                 .fillMaxWidth()
+                .navigationBarsPadding()
                 .imePadding(),
         color = IrisTheme.palette.toolbar,
         tonalElevation = 0.dp,
